8364100dba6ccd247a6df1582041829180c4fbab
woowooyong <woowooyong@gmail.com>
2023-06-06 13:43:44 +0800

    update: 測試vercel edge fn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
-  nitro: {
-    preset: 'vercel-edge'
-  },
   typescript: {
     typeCheck: true
   },
